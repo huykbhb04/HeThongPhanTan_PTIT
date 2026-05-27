0f4c7a62557ca367afc040dbe061025f95ccb64b
huykbhb04 <HuyBD.B22CN375@stu.ptit.edu.vn>
2026-05-28 01:06:00 +0700

    Fix: pass documentId prop to Editor component

--- src/app/documents/[documentId]/document.tsx
+++ src/app/documents/[documentId]/document.tsx
@@ -23,7 +23,7 @@ export const Document = ({ preloadedDocument }: DocumentProps) => {
           <Toolbar />
         </div>
         <div className="pt-[114px] print:pt-0">
-          <Editor initialContent={document.initialContent} />
+          <Editor initialContent={document.initialContent} documentId={document._id} />
         </div>
       </div>
     </Room>
